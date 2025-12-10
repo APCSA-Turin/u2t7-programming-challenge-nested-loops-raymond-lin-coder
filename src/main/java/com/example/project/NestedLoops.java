4511c2224bdd5e95687600a5c52e8c308a57dd9a
Raymond Lin <raymondl183@nycstudents.net>
2025-12-10 18:32:10 +0000

    First commit

--- src/main/java/com/example/project/NestedLoops.java
+++ src/main/java/com/example/project/NestedLoops.java
@@ -17,7 +17,16 @@ return the string below for given height=5
 *****  
            */
 public static String starStaircase(int height){
-    return "";
+    String answer = "";
+for (int i = 1; i < height + 1; i++) {
+  for (int j = 1; j <= i; j++) {
+    answer += ("*");
+  }
+  if (i != height) {
+  answer += "\n";
+  }
+}
+    return answer;
 }
           /*
 return the string below for given height=5
@@ -29,7 +38,16 @@ return the string below for given height=5
            */
 
 public static String starStaircaseReverse(int height){
-    return "";
+        String answer = "";
+    for (int i = height; i > 0; i--) {
+  for (int j = i; j > 0; j--) {
+    answer += "*";
+  }
+  if (i != 1) {
+  answer += "\n";
+  }
+}
+    return answer;
 }
             /*
 
@@ -41,7 +59,23 @@ width=5 height=5
 *****
           */
     public static String emptyBox(int width, int height) {
-        return "";
+        String answer = "";
+        if (width < 2 || height < 2) {
+            return "";
+        }
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= width; j++) {
+                if (i == 1 || i == height || j == 1 || j == width) {
+                    answer += "*";
+                } else {
+                    answer += " ";
+                }
+            }
+            if (i != height) {
+                answer += "\n";
+            }
+        }
+        return answer;
     }
                     /*
 return the string below for word="HELLO" rows=5                    
@@ -54,7 +88,17 @@ OHELL
   i will provide a hint for this one if you get stuck.. use modulo
                 */
     public static String repeatRectangle(String word, int rows){
-        return "";
+        String answer = "";
+        int length = word.length();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < length; j++) {
+                answer += word.charAt((i + j) % length);
+            }
+            if (i != rows - 1) {
+                answer += "\n";
+            }
+        }
+        return answer;
     }
 
 }
--- src/main/java/com/example/project/NestedLoopsRunner.java
+++ src/main/java/com/example/project/NestedLoopsRunner.java
@@ -2,7 +2,7 @@ package com.example.project;
 
 public class NestedLoopsRunner {
     public static void main(String[] args) {
-     
+     System.out.println(NestedLoops.repeatRectangle("HELLO", 10));
     }
 }
 
